9cc5e53608bb56f817849f0144e1e119aab96c0a
Shine Htet <40858556@live.napier.ac.uk>
2026-09-23 16:23:56 +0630

    Added "The End" text output in Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -13,5 +13,7 @@ public class Main {
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
             IO.println("i = " + i);
         }
+
+        IO.println("The End");
     }
 }
